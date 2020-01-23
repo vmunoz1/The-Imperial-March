3dc5b43d5221fd2d4da00086a11a0fe8f51d79a9
vmunoz1 <45017690+vmunoz1@users.noreply.github.com>
2020-01-23 11:23:14 -0800

    Add files via upload

---
+++ Imperial.rb
@@ -0,0 +1,123 @@
+use_bpm 50
+live_loop :imperial do
+  3.times do
+    play:G4, rate:3
+    sleep 0.6
+  end
+  play:E4
+  sleep 0.3
+  play:B4
+  sleep 0.3
+  play:G4
+  sleep 0.6
+  play:E4
+  sleep 0.3
+  play:B4
+  sleep 0.3
+  play:G4
+  sleep 0.6
+  3.times do
+    play:D5
+    sleep 0.6
+  end
+  play:E5
+  sleep 0.3
+  play:B4
+  sleep 0.3
+  play:G4
+  sleep 0.6
+  play:E4
+  sleep 0.3
+  play:B4
+  sleep 0.3
+  play:G4
+  sleep 0.6
+  play:G5
+  sleep 0.6
+  play:G4
+  sleep 0.6
+  play:G4
+  sleep 0.6
+  play:G5
+  sleep 0.6
+  play:G5
+  sleep 0.3
+  play:F5
+  sleep 0.3
+  play:E5
+  sleep 0.3
+  play:D5
+  sleep 0.3
+  play:E5
+  sleep 0.3
+  play:G4
+  sleep 0.3
+  play:C5
+  sleep 0.6
+  play:B4
+  sleep 0.3
+  play:B4
+  sleep 0.3
+  play:B4
+  sleep 0.6
+  play:A4
+  sleep 0.3
+  play:B4
+  sleep 0.3
+  play:E4
+  sleep 0.6
+  play:G4
+  sleep 0.6
+  play:E4
+  sleep 0.3
+  play:G4
+  sleep 0.3
+  play:B4
+  sleep 0.6
+  play:G4
+  sleep 0.3
+  play:D5
+  sleep 0.6
+  play:G5
+  sleep 0.6
+  play:G4
+  sleep 0.3
+  play:G4
+  sleep 0.3
+  play:G5
+  sleep 0.6
+  play:G5
+  sleep 0.3
+  play:F5
+  sleep 0.6
+  play:E5
+  sleep 0.3
+  play:D5
+  sleep 0.3
+  play:E5
+  sleep 0.3
+  play:G4
+  sleep 0.6
+  play:C5
+  sleep 0.6
+  play:B4
+  sleep 0.3
+  play:B4
+  sleep 0.6
+  play:B4
+  sleep 0.3
+  play:A4
+  sleep 0.3
+  play:B4
+  sleep 0.3
+  play:E4
+  sleep 0.6
+  play:G4
+  sleep 0.6
+  2.times do
+    play:E4
+    sleep 0.3
+    play:B4
+    sleep 0.6
+  end
+end
